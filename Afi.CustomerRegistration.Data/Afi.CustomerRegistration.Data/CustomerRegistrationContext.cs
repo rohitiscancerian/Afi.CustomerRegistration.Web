41106f19de8c1a84c28ccc50159fdbc365728e23
Rohit Kumar <rohit.kumar@arup.com>
2020-11-21 14:28:53 +0000

    Add project files.

---
+++ Afi.CustomerRegistration.Data/Afi.CustomerRegistration.Data/CustomerRegistrationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+
+namespace Afi.CustomerRegistration.Data
+{
+    public class CustomerRegistrationContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Data\CustomerRegistration.sqlite");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Afi.CustomerRegistration.Web/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Afi.CustomerRegistration.Web.Models;
+
+namespace Afi.CustomerRegistration.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMediator _mediatr;
+
+        public CustomerController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] CreateCustomerRequest request)
+        {
+
+        }
+    }
+}
